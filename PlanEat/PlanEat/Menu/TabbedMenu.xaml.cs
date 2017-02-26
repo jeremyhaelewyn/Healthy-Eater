e181debf13c952cc1298b965882d77dad4f3cbc6
jeremyhaelewyn <jeremha18@gmail.com>
2017-02-26 15:43:46 +0100

    Create de la solution
    + test TabbedPage

---
+++ PlanEat/PlanEat/Menu/TabbedMenu.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace PlanEat
+{
+	public partial class TabbedMenu : TabbedPage
+	{
+		public TabbedMenu()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ PlanEat/PlanEat/Views/Aujourdhui.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace PlanEat
+{
+	public partial class Aujourdhui : ContentPage
+	{
+		public Aujourdhui()
+		{
+			InitializeComponent();
+		}
+	}
+}
